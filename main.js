6544f8d320262baa3bc16dfaa486707ccbac6297
wingkeet <leongwingkeet@gmail.com>
2020-06-03 17:37:28 +0800

    Remove comments regarding color usage

--- main.js
+++ main.js
@@ -19,22 +19,7 @@ async function main() {
     answer = await ask(choices)
     console.log(choices[answer])
 
-    // Example 2 with all options specified.
-    // Colors may be specified using:
-    //   - a Number (8-bit color; e.g. { color: 6 } for cyan), or
-    //   - a String (24-bit RGB color; see below for details)
-    // For 8-bit color, refer to the lookup table at
-    //   https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
-    // For 24-bit color, CSS named-colors, hex-colors, rgb-colors and hsl-colors are supported.
-    //   Examples: - { color: 'CornflowerBlue' }
-    //             - { color: '#fe10ca' }, { color: '#d2b' }
-    //             - { color: 'rgb(188,0,95)' }, { color: 'rgb(0%,50%,100%)' }
-    //             - { color: 'hsl(0,100%,50%)' }
-    // All color values are case-insensitive.
-    // Spaces should not be used within the color values.
-    // Invalid color values are silently ignored.
-    // A list of CSS color names can be found at
-    //   https://www.w3schools.com/cssref/css_colors.asp
+    // Example 2 with all options specified
     choices = [
         'Abkhazia',
         'Afghanistan',
